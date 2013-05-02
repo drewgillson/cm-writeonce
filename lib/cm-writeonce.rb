fa2c76378deb065cf9012a53ed539d38a573357b
Drew Gillson <drew.gillson@gmail.com>
2013-05-02 14:26:22 -0600

    We can now add and update subscribers using this tool

--- lib/cm-writeonce.rb
+++ lib/cm-writeonce.rb
@@ -1,79 +1,71 @@
-require "net/http"
-require "cgi"
 require "cm-writeonce/version"
 
 module CmWriteonce
     
     require 'createsend'
-    require 'date'
 
-    def self.initialize(cm_key, cm_list)
+    def self.initialize(cm_key, cm_list, filename)
         @cm_key = cm_key
         @cm_list = cm_list
         @now = DateTime.now.to_time.to_i
         @auth = {:api_key => @cm_key}
         cs = CreateSend::CreateSend.new @auth
 
-        @list = CreateSend::List.new @auth, @cm_list
+        i = 0
+        File.open(filename).read.split("\n").each do |line|
+          begin
+              name, email_address, street_address, city, province, country, postal_code, telephone, initial_source, campaign, gender, language_preference = line.split(",")
 
-        records = @list.active '1970-01-01', 1, 10
-        puts records.inspect
-    end
+              if i == 0
+                  raise "Could not find correct headers in CSV file!" unless name == "Name" &&
+                                                                  email_address == "Email Address" &&
+                                                                  street_address == "Street Address" &&
+                                                                  city == "City" &&
+                                                                  province == "Province" &&
+                                                                  country == "Country" &&
+                                                                  postal_code == "Postal Code" &&
+                                                                  telephone == "Telephone" &&
+                                                                  initial_source == "Initial Source" &&
+                                                                  campaign == "Campaign" &&
+                                                                  gender == "Gender" &&
+                                                                  language_preference == "Language Preference"
+              else
+                  email_address.downcase!
+                  postal_code.upcase!
+                  postal_code = postal_code.gsub(/\s+/, "")
+                  province = province.upcase!
+                  country = country.split(' ').map(&:capitalize).join(' ')
+                  telephone = telephone.gsub(/([-() ])/, '')
+                  gender.capitalize!
+                  language_preference.upcase!
+                  custom_fields = [ { :Key => 'Street Address', :Value => street_address, :Clear => false },
+                                    { :Key => 'City', :Value => city, :Clear => false },
+                                    { :Key => 'Province', :Value => province, :Clear => false },
+                                    { :Key => 'Country', :Value => country, :Clear => false },
+                                    { :Key => 'Postal Code', :Value => postal_code, :Clear => false },
+                                    { :Key => 'Telephone', :Value => telephone, :Clear => false },
+                                    { :Key => 'Campaign', :Value => campaign, :Clear => false },
+                                    { :Key => 'Gender', :Value => gender, :Clear => false },
+                                    { :Key => 'Language Preference', :Value => language_preference, :Clear => false } ]
+                  begin
+                      details = CreateSend::Subscriber.new @auth, @cm_list, email_address
+                      details.update email_address, name, custom_fields, true
+                      puts "Updated " << name << " <" << email_address << ">"
+                  rescue Exception => e
+                      if e.message == 'The CreateSend API responded with the following error - 203: Subscriber not in list or has already been removed.'
+                          custom_fields << { :Key => 'Initial Source', :Value => initial_source }
+                          email_address = CreateSend::Subscriber.add @auth, @cm_list, email_address, name, custom_fields, true
+                          puts "Added " << name << " <" << email_address << ">"
+                      end
+                  end
+              end
 
-=begin
-    def self.record(d)
-        p = d['p']
-        type = d['s']
-        email = p.EmailAddress
-
-        begin
-            k = KM.new
-            k.init(@km_key, :log_dir => 'log/')
-            k.identify(email)
-
-            details = CreateSend::Subscriber.get @auth, @cm_list, email
-            ts = DateTime.parse(details.Date).to_time.to_i
-            
-            case type
-                when 'active' then label = 'CM subscribed to email'
-                when 'unsubscribed' then label = 'CM unsubscribed from email'
-                when 'bounced' then label = 'CM bounced from email'
-                when 'deleted' then label = 'CM deleted from email'
-            end
-            
-            k.record(label, {'List' => @list.details.Title, '_d' => 1, '_t' => ts})
-            details.CustomFields.each{|field|
-                if ['City','Province','Postal Code','Source'].include? field.Key
-                    k.set({field.Key => field.Value}) if field.Value != '' && field.Value != nil
-                end
-            }
-
-            subscriber_events = []
-            person = CreateSend::Subscriber.new @auth, @cm_list, email
-            history = person.history
-            history.each{|h|
-                h.Actions.each{|a|
-                    ts = DateTime.parse(a.Date).to_time.to_i
-                    if @now - ts <= (@allowed_history_days * 86400) || @allowed_history_days == 0
-                        if a.Event == 'Open'
-                            data = {'Email' => h.Name, '_d' => 1, '_t' => ts}
-                            subscriber_events << Thread.new(data){|data| k.record('CM opened an email', data)}
-                        elsif a.Event == 'Click'
-                            data = {'Email' => h.Name, 'Link' => a.Detail, '_d' => 1, '_t' => ts}
-                            subscriber_events << Thread.new(data){|data| k.record('CM clicked something in an email', data)}
-                        end
-                    end
-                }
-            }
-            subscriber_events.each { |t| t.join }
-            subscriber_events.each { |t| t.exit }
-            person = nil
-            history = nil
-            details = nil
-            puts "Finished recording events for #{email}"
-        rescue StandardError => e
-            puts e.message
+              i = i + 1
+          rescue Exception => e 
+              puts e.message
+              puts "Expecting: Name,Email Address,Address,City,Province,Country,Postal Code,Telephone,Initial Source,Campaign,Gender,Language Preference"
+              abort
+          end 
         end
     end
-=end
 end
